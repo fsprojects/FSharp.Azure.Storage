b3af67b66fe103eae2c0fcc6c27895a7bff1dcc5
Daniel Chambers <daniel@digitallycreated.net>
2014-04-12 16:56:55 +1000

    Added assembly info attributes and a nuspec file ready for nuget packaging

---
+++ FSharp.Azure/AssemblyInfo.fs
@@ -0,0 +1,14 @@
+﻿namespace DigitallyCreated.FSharp.Azure
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("DigitallyCreated.FSharp.Azure")>]
+[<assembly: AssemblyCompany("Daniel Chambers & Contributors")>]
+[<assembly: AssemblyProduct("FSharp.Azure")>]
+[<assembly: AssemblyCopyright("Copyright © Daniel Chambers & Contributors 2014")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.0-alpha")>]
+
+do()
