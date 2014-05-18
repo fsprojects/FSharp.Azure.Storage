8dd228605c06c1b38f5fe5aabdd3c96fca1bcc89
Daniel Chambers <daniel@digitallycreated.net>
2014-05-18 23:48:04 +1000

    Updated assembly version to 1.0.0

--- FSharp.Azure/AssemblyInfo.fs
+++ FSharp.Azure/AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Runtime.CompilerServices
 
 [<assembly: AssemblyVersion("1.0.0.0")>]
 [<assembly: AssemblyFileVersion("1.0.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.0-beta3")>]
+[<assembly: AssemblyInformationalVersion("1.0.0")>]
 
 [<assembly: InternalsVisibleTo("DigitallyCreated.FSharp.Azure.IntegrationTests")>]
 
