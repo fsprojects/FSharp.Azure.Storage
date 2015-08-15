187e453905fae2997b4eda555e32e25b59368c96
Daniel Chambers <daniel@digitallycreated.net>
2015-08-15 22:29:10 +1000

    Added deprecated message to NuGet package in preparation to move to a new package name. Bump version to 1.0.3.

--- FSharp.Azure/AssemblyInfo.fs
+++ FSharp.Azure/AssemblyInfo.fs
@@ -8,9 +8,9 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyProduct("FSharp.Azure")>]
 [<assembly: AssemblyCopyright("Copyright © Daniel Chambers & Contributors 2015")>]
 
-[<assembly: AssemblyVersion("1.0.2.0")>]
-[<assembly: AssemblyFileVersion("1.0.2.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.2")>]
+[<assembly: AssemblyVersion("1.0.3.0")>]
+[<assembly: AssemblyFileVersion("1.0.3.0")>]
+[<assembly: AssemblyInformationalVersion("1.0.3")>]
 
 [<assembly: InternalsVisibleTo("DigitallyCreated.FSharp.Azure.IntegrationTests")>]
 
