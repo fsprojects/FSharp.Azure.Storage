58f60f170a441230e6829fb342fe73b825ecaa9e
Daniel Chambers <daniel@digitallycreated.net>
2018-05-20 00:07:17 +1000

    Using all uppercase environment variable names

--- test/FSharp.Azure.Storage.Tests/Program.fs
+++ test/FSharp.Azure.Storage.Tests/Program.fs
@@ -5,11 +5,15 @@ open Expecto
 
 [<EntryPoint>]
 let main argv =
-    let connStrEnvVar = Environment.GetEnvironmentVariable "FSharpAzureStorageConnectionString"
+    let connStrEnvVar = Environment.GetEnvironmentVariable "FSHARP_AZURE_STORAGE_CONNECTION_STRING"
     let connectionString =
         if String.IsNullOrWhiteSpace connStrEnvVar
-        then "UseDevelopmentStorage=true;"
-        else connStrEnvVar
+        then
+            do printfn "Using Development Storage for tests"
+            "UseDevelopmentStorage=true;"
+        else
+            do printfn "Using connection string from FSHARP_AZURE_STORAGE_CONNECTION_STRING env var for tests"
+            connStrEnvVar
 
     runTestsWithArgs defaultConfig argv <|
         testList "FSharp.Azure.Storage" [
